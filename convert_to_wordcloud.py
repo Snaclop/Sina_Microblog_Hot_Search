2d2114806346ebba1d126fcab64b3bde0f32880a
Snaclop <billimachavior@163.com>
2025-03-19 09:56:27 +0000

    Update convertion files

--- convert_to_wordcloud.py
+++ convert_to_wordcloud.py
@@ -1,12 +1,15 @@
 import jieba as jb
 import wordcloud as wc
-import pandas as pd
+
 
 #  读取hot_search.csv文件
 
+with open("Hot_search.csv", mode="r", encoding="utf-8") as file:
+    pass
 
 
-result_list = jb.lcut(search)
+result_list = jb.lcut(search, cut_all=False)
+print(result_list)
 
-stopwords = ["的", "是", "说", "称", "了", "你"]
+stopwords = ["的", "是", "说", "称", "了", "你", " "]
 
