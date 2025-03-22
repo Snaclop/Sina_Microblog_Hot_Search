781585b474fd83164b737dad257bee1d10a508c2
Snaclop <billimachavior@163.com>
2025-03-22 13:14:48 +0000

    Archieve Feb data

--- convert_to_wordcloud.py
+++ convert_to_wordcloud.py
@@ -5,12 +5,13 @@ import csv
 #  读取hot_search.csv文件
 
 data = []
-with open("Hot_search.csv", mode="r", encoding="utf-8") as file:
+with open("Archieved_m2.csv", mode="r", encoding="utf-8") as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         data.append(row[1])
     print(data)
 
+#  使用jieba进行分词
 
 stopwords = ["的", "是", "说", "称", "了", "你", " "]
 
