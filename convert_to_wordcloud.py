514d874ed42a7fd724a6d0258e63207c5ac02f6f
Snaclop <billimachavior@163.com>
2025-03-23 23:05:01 +0800

    Update Feb wordcloud image

--- convert_to_wordcloud.py
+++ convert_to_wordcloud.py
@@ -1,17 +1,30 @@
 import jieba as jb
-import wordcloud as wc
+from wordcloud import WordCloud 
 import csv
 
 #  读取hot_search.csv文件
 
 data = []
-with open("Archieved_m2.csv", mode="r", encoding="utf-8") as file:
+with open("Archieved_feb.csv", mode="r", encoding="utf-8") as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         data.append(row[1])
-    print(data)
 
 #  使用jieba进行分词
 
-stopwords = ["的", "是", "说", "称", "了", "你", " "]
+words_list = []
+stopwords = [' ', '男子', '女子', '被', '的', '了', '在', '是', '我', '有', '和', '就', '不', '人', '都', '一', '一个', '上', '也', '很', '到', '说', '要', '去', '你', '会', '着', '没有', '看', '好', '自己', '这', '个', '秒', '最', '或', '多', '过', '太', '请', '做', '想', '当', '只']
+for item in data:
+    ret = jb.lcut(item)
+    for word in ret:
+        if word not in stopwords:
+            words_list.append(word)
 
+#  生成词云图
+
+words = ' '.join(words_list)
+
+font_path = 'C:/Windows/Fonts/simhei.ttf'
+w = WordCloud(font_path=font_path, height=800, width=1000)
+w.generate(str(words))
+w.to_file(rf'./assets/feb.png')
